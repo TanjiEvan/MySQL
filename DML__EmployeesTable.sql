852fa57d4aad76856c3e2eb2db8597568429894d
Tanjib Ul Islam <tanji.evan20@gmail.com>
2024-07-11 23:43:02 +0600

    Update DML__EmployeesTable.sql

--- DML__EmployeesTable.sql
+++ DML__EmployeesTable.sql
@@ -380,6 +380,80 @@ SELECT dept_no,dept_name,coalesce(dept_manager,dept_name, "N/A") AS dept_manager
 SELECT dept_no,dept_name,COALESCE("Dept manager name") AS fake_col FROM departments_dup;
 #ROLLBACK;
 ## TASK ##
+SELECT dept_no,dept_name, coalesce(dept_no,dept_name) AS dept_info 
+FROM departments_dup ORDER BY dept_no ASC;
+
+SELECT 
+IFNULL(dept_no,"N/A") as dept_no,
+IFNULL(dept_name,"Dept Name Not provided") AS dept_name,
+COALESCE(dept_no,dept_name) AS dept_info
+FROM departments_dup ORDER BY dept_no ASC;
+
+														#### SQL JOINS ####
+## TASK ##
+SELECT * FROM departments_dup;
+
+ALTER TABLE departments_dup
+DROP COLUMN dept_manager;
+ALTER TABLE departments_dup
+DROP COLUMN trying;
+
+ALTER TABLE departments_dup
+CHANGE COLUMN dept_no 
+dept_no CHAR(4) NULL;
+
+ALTER TABLE departments_dup
+CHANGE COLUMN dept_name dept_name VARCHAR(40) NULL;
+
+DESCRIBE departments_dup;
+COMMIT;
+
+INSERT INTO departments_dup(dept_name)
+VALUES ("Public Relations");
+
+DELETE FROM departments_dup
+WHERE dept_no="d002";
+
+INSERT INTO departments_dup(dept_no)
+VALUES ("d010"),("d011");
+
+SELECT * FROM departments_dup;
+
+
+CREATE TABLE dept_manager_dup
+( 
+emp_no int (11) NOT NULL,
+dept_no char(4) NULL,
+from_date date NOT NULL,
+to_date date NULL
+);
+
+
+-- Insert data into dept_manager_dup from dept_manager
+INSERT INTO dept_manager_dup
+SELECT * FROM dept_manager;
+
+-- Insert specific rows into dept_manager_dup
+INSERT INTO dept_manager_dup (emp_no, from_date)
+VALUES
+    (999904, '2017-01-01'),
+    (999905, '2017-01-01'),
+    (999906, '2017-01-01'),
+    (999907, '2017-01-01');
+
+-- Delete rows from dept_manager_dup where dept_no is 'd001'
+DELETE FROM dept_manager_dup
+WHERE dept_no = 'd001';
+
+-- Insert a new department into departments_dup
+INSERT INTO departments_dup (dept_name)
+VALUES ('Public Relations');
+
+-- Delete rows from departments_dup where dept_no is 'd002'
+DELETE FROM departments_dup
+WHERE dept_no = 'd002';
+
+
 
 
 
