09ca97468875482f3f29853093382762a0a92f40
Tanjib Ul Islam <tanji.evan20@gmail.com>
2024-07-12 23:57:40 +0600

    added LEFT JOIN

--- DML__EmployeesTable.sql
+++ DML__EmployeesTable.sql
@@ -519,7 +519,42 @@ GROUP BY m.emp_no
 ORDER BY m.emp_no;
 
 
+															#### LEFT JOIN ####
+  ## Delete the duplicates 
+DELETE FROM dept_manager_dup
+WHERE emp_no='110228';
+DELETE FROM departments_dup
+WHERE dept_no='d009';
 
+## Add back the initial records 
+INSERT INTO dept_manager_dup
+VALUES('110228','d003','1992-03-21','9999-01-01');
+INSERT INTO departments_dup
+VALUES ('d009','Customer Service');
+                                                                    
+ SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        LEFT JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+ORDER BY m.dept_no;                                                                   
+  
+## What if we switch the table ??
+
+SELECT 
+    d.dept_no, m.emp_no, d.dept_name
+FROM
+    departments_dup d
+        LEFT JOIN
+    dept_manager_dup m ON m.dept_no = d.dept_no
+ORDER BY d.dept_no;                                                                   
+                         
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
 
                                                
                                                                 
