aaafdf88eeea68792642b064d4baf33112d3a4a9
Tanjib Ul Islam <tanji.evan20@gmail.com>
2024-07-13 23:52:56 +0600

    Added Subqueries

--- DML__EmployeesTable.sql
+++ DML__EmployeesTable.sql
@@ -640,17 +640,127 @@ ORDER BY e.emp_no;
 ##Technical Execution 
 SET @@global.sql_mode = REPLACE(@@global.sql_mode, 'ONLY_FULL_GROUP_BY', '');
 
+							#### SQL Subqueries With IN inside WHERE ####
+SELECT * FROM dept_manager;
 
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+    e.emp_no IN (SELECT 
+            dm.emp_no
+        FROM
+            dept_manager dm);
 
+## TASK ##
+SELECT 
+    *
+FROM
+    dept_manager 
+WHERE
+    emp_no IN (SELECT 
+            emp_no
+        FROM
+            employees 
+        WHERE
+            hire_date BETWEEN '1990-01-01' AND '1995-01-01');
 
+### EXISTS- NOT EXISTS in WHERE ##
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            dept_manager dm
+        WHERE
+            dm.emp_no = e.emp_no);
 
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            dept_manager dm
+        WHERE
+            dm.emp_no = e.emp_no)
+            ORDER BY emp_no;
+## TASK ##
 
+SELECT 
+    *
+FROM
+    employees e
+WHERE
+     EXISTS (SELECT 
+            *
+        FROM
+            titles t
+        WHERE
+        t.emp_no=e.emp_no AND
+            title = 'Assistant Engineer');
 
+### Nested in SELECT & FROM ###
 
+## TASK :- Assign employee number 110022 as a manager to all employees from 10001 to 10020, 
+#           and employee number 110039 as a manager to all employees from 10021 to 10040.
 
+SELECT emp_no FROM employees WHERE emp_no = 110022;
 
+SELECT 
+    A.*
+FROM
+    (SELECT 
+        e.emp_no AS employees_id,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    employees
+                WHERE
+                    emp_no = 110022) AS manager_id
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS A 
+UNION SELECT 
+    B.*
+FROM
+    (SELECT 
+        e.emp_no AS employees_id,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    employees
+                WHERE
+                    emp_no = 110039) AS manager_id
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no >= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no
+    LIMIT 20) AS B;
 
+## TASK ##
+DROP TABLE IF EXISTS emp_manager;
+CREATE TABLE emp_manager
+(emp_no INT(11) NOT NULL,
+dept_no CHAR(4) NULL,
+manager_no INT(11) NOT NULL);
 
+SELECT * FROM emp_manager;
 
 
 
