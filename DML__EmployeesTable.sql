110585261f7b0038f36a4cecaa9d10f2f5e4adbb
Tanjib Ul Islam <tanji.evan20@gmail.com>
2024-07-13 22:41:31 +0600

    Completed SQL JOINS

--- DML__EmployeesTable.sql
+++ DML__EmployeesTable.sql
@@ -588,10 +588,64 @@ FROM
     departments_dup d ON m.dept_no = d.dept_no
 ORDER BY dept_no;                                                                       
 
-                                               
-                                                                
-                                                                
-                                                                
+										#### The New and The Older JOIN Syntax ####                                              
+ # WHERE                                                               
+ SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m,
+    departments_dup d
+WHERE
+    m.dept_no = d.dept_no
+ORDER BY dept_no;
+
+## TASK ##
+SELECT 
+    e.emp_no, e.first_name, e.last_name, dm.dept_no, e.hire_date
+FROM
+    employees e,
+    dept_manager dm
+WHERE
+    e.emp_no = dm.emp_no;
+ #-------------------------#                                                               
+SELECT 
+    e.emp_no, e.first_name, e.last_name, dm.dept_no, e.hire_date
+FROM
+    employees e
+        INNER JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no;
+
+									#### JOIN & WHERE USED TOGETHER ####                            
+SELECT 
+    e.emp_no, e.first_name, e.last_name, s.salary
+FROM
+    employees e
+        INNER JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    salary > 145000;
+
+## TASK ##
+SELECT 
+    e.emp_no, e.first_name, e.last_name, e.hire_date, t.title
+FROM
+    employees e
+        LEFT JOIN
+    titles t ON e.emp_no = t.emp_no
+WHERE
+    first_name = 'Margareta'
+        AND last_name = 'Markovitch'
+ORDER BY e.emp_no;
+
+##Technical Execution 
+SET @@global.sql_mode = REPLACE(@@global.sql_mode, 'ONLY_FULL_GROUP_BY', '');
+
+
+
+
+
+
+
 
 
 
