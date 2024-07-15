35c867d28c1300b7e0c0c90213e5ef48e73f6015
Tanjib Ul Islam <tanji.evan20@gmail.com>
2024-07-15 20:24:31 +0600

    Added Variables & User-Defined-Functions

--- DML__EmployeesTable.sql
+++ DML__EmployeesTable.sql
@@ -997,7 +997,75 @@ BEGIN
 END $$
 DELIMITER ;
 
+														#### SQL Variables ####
+SET @v_avg_salary=0 ;
+CALL employees.emp__avg_salary_out(11300, @v_avg_salary);
+SELECT @v_avg_salary;
+## TASK ##
+SET @v_emp_no=0;
+CALL emp_info("Aruna","Journel",@v_emp_no);
+SELECT @v_emp_no;
 
+												#### USER DEFIINED FUNCTIONS ####
+USE employees;
+DROP PROCEDURE IF EXISTS f_emp_avg_salary;
+
+DELIMITER $$
+CREATE FUNCTION f_emp_avg_salary(p_emp_no INTEGER) RETURNS DECIMAL (10,2)
+NO SQL
+BEGIN
+DECLARE v_avg_salary DECIMAL(10,2);
+
+SELECT
+AVG(s.salary)
+INTO v_avg_salary
+FROM employees e
+JOIN 
+salaries S ON e.emp_no=s.emp_no
+WHERE e.emp_no=p_emp_no;
+
+RETURN v_avg_salary;
+END $$
+
+DELIMITER ;
+
+SELECT f_emp_avg_salary(11300);
+
+## TASK ##
+
+USE employees;
+DROP PROCEDURE IF EXISTS f_emp_avg_salary;
+
+DELIMITER $$
+CREATE FUNCTION emp_info(p_first_name VARCHAR(255),p_last_name VARCHAR(255)) RETURNS DECIMAL(10,2)
+NO SQL
+BEGIN 
+DECLARE v_max_from_date date;
+DECLARE v_salary DECIMAL (10,2);
+
+SELECT
+MAX(from_date)
+INTO v_max_from_date FROM employees e
+JOIN
+salaries s ON e.emp_no= s.emp_no
+WHERE
+e.first_name = p_first_name
+AND e.last_name= p_last_name;
+
+SELECT s.salary INTO v_salary FROM
+employees e
+JOIN
+salaries s ON e.emp_no = s.emp_no
+WHERE 
+e.first_name = p_first_name
+AND e.last_name= p_last_name
+AND s.from_date = v_max_from_date;
+RETURN v_salary;
+
+END $$
+DELIMITER ;
+
+SELECT emp_info("Aruna","Journel");
 
 
 
